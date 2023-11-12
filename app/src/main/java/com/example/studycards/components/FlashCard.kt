5ed9f1454d140408c0c92e5e83397654ce041f34
SatchelF <satchcollege@gmail.com>
2023-11-12 16:46:56 -0700

    flipping logic

--- app/src/main/java/com/example/studycards/components/FlashCard.kt
+++ app/src/main/java/com/example/studycards/components/FlashCard.kt
@@ -1,103 +1,127 @@
 package com.example.studycards.components
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.animation.Animatable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.consumeAllChanges
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.studycards.models.Flashcard
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.geometry.isUnspecified
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
 fun Flashcard(flashcard: Flashcard) {
-    val scope = rememberCoroutineScope()
-
-    // Rotation degrees of the card
     val rotation = remember { Animatable(0f) }
+    val isFlipped = remember { mutableStateOf(false) }
 
-    // Whether the card is flipped
-    var isFlipped by remember { mutableStateOf(false) }
-
-    // Gesture modifier for handling drag gestures
-    val gestureModifier = Modifier.pointerInput(Unit) {
-        detectHorizontalDragGestures(
-            onDragStart = {
-                // This is where we can set state when the user starts dragging
-            },
-            onDragEnd = {
-                // When the user stops dragging
-                if (!isFlipped) {
-                    scope.launch {
-                        rotation.animateTo(0f, tween(500))
-                    }
-                }
-            },
-            onHorizontalDrag = { change, dragAmount ->
-                scope.launch {
-                    // Calculate the potential new rotation value
-                    val newRotation = rotation.value + dragAmount * 0.2f // Adjust this multiplier for sensitivity
-
-                    // Check if we've reached the threshold to flip the card
-                    if (newRotation <= -90f && !isFlipped) {
-                        // Card is now flipped to back
-                        isFlipped = true
-                        rotation.snapTo(90f) // Snap rotation to start showing the back
-                    } else if (newRotation >= 90f && isFlipped) {
-                        // Card is now flipped to front
-                        isFlipped = false
-                        rotation.snapTo(-90f) // Snap rotation to start showing the front
-                    } else {
-                        // If the thresholds are not reached, just update the rotation value
-                        rotation.snapTo(newRotation)
-                    }
-                }
-                change.consumeAllChanges()
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+        ),
+        modifier = Modifier
+            .fillMaxWidth(0.90f)
+            .padding(top = 200.dp)
+            .height(300.dp)
+            .padding(vertical = 16.dp)
+            .graphicsLayer {
+                rotationY = rotation.value
             }
-        )
-    }
+            .pointerInput(Unit) {
+                coroutineScope {
+                    while (true) {
+                        val pointerId = awaitPointerEventScope {
+                            awaitFirstDown().id
+                        }
 
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        Card(
+                        rotation.stop()
+                        awaitPointerEventScope {
+                            horizontalDrag(pointerId) { change ->
+                                val positionChange = change.positionChange()
+                                if (!positionChange.isUnspecified) {
+                                    launch {
+                                        val newRotation = rotation.value + positionChange.x / 10f
+                                        rotation.snapTo(clamp(newRotation))
+
+                                        // Check and flip card
+                                        when {
+                                            !isFlipped.value && newRotation <= -90f -> {
+                                                isFlipped.value = true
+                                                rotation.snapTo(90f) // Swap to back text
+                                            }
+                                            isFlipped.value && newRotation >= 90f -> {
+                                                isFlipped.value = false
+                                                rotation.snapTo(-90f) // Swap to front text
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        // Animate back to front if swipe is incomplete
+                        launch {
+                            rotation.animateTo(0f, animationSpec = spring(.75f, 50f))
+                        }
+                    }
+                }
+            }
+    ) {
+        Column(
             modifier = Modifier
-                .height(300.dp)
-                .fillMaxWidth(0.95f)
-                .graphicsLayer {
-                    // Correctly display the text based on the flip state
-                    rotationY = if (isFlipped) rotation.value - 180f else rotation.value
-                }
-                .then(gestureModifier) // Apply the gesture modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            Box(contentAlignment = Alignment.Center) {
-                Text(
-                    text = if (!isFlipped) flashcard.front else flashcard.back,
-                    style = MaterialTheme.typography.headlineMedium,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
+            Text(
+                text = flashcard.type,
+                modifier = Modifier.align(Alignment.Start),
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = if (isFlipped.value) flashcard.back else flashcard.front,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                textAlign = TextAlign.Start,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
 
-
-
-
-
-
+fun clamp(value: Float, min: Float = -180f, max: Float = 180f): Float {
+    return when {
+        value >= max -> max
+        value <= min -> min
+        else -> value
+    }
+}
